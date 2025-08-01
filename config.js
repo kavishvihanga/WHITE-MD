25f8827e8a6f16957ea86e822159d20e5ae8ab28
kavishvihanga <vihangakavish19940928@gmail.com>
2025-08-01 18:16:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "LIFwgQLK#Fb5NAvr2yK8n8hzu5eWyRwjm1-zIlUkAsFIdbE_fdw4",
+SESSION_ID: process.env.SESSION_ID || "yc0hWLDQ#rB84Q2Jmt2yKd2TvWlzz8aUaImQwbiS7DdqS2P9RKAg",
 ALIVE_IMG: process.env.ALIVE_IMG || "Enter The Image URL",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM WHITE MD CREATED BY HASHAN <NOW ALIVE> ",
 };
